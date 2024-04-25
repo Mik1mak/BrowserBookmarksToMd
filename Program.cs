cd78f378bcaa64b3ac304aaa259721c263868b05
Mik1mak <mikolaj.mak00@gmail.com>
2024-04-25 13:34:02 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.CommandLine;
+
+var filePathArg = new Argument<FileInfo>("Bookmarks", "File with browser bookmarks");
+
+var rootCommand = new RootCommand("Converts HTML file with browser bookmarks to folders and Markdown files.")
+{
+    filePathArg
+};
+
+rootCommand.SetHandler((FileInfo bookmarks) => { 
+    List<DirectoryInfo> createdDictionaries = new();
+    List<FileInfo> createdFiles = new();
+
+    // TODO: handler;
+    
+}, filePathArg);
+
+await rootCommand.InvokeAsync(args);
